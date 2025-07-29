e57393cc08fe59ca05acca699f0b49010d7abcd5
eitanabrishami <eitanabrishami@gmail.com>
2025-07-29 16:58:44 -0700

    Intial Commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,82 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import {Text, View} from "react-native";
+import {Tabs} from "expo-router";
+import {ImageBackground} from "react-native";
+import {Image} from "react-native";
+import {icons} from "@/constants/icons"; // Import the icons
+import {images} from "@/constants/images";
+import React from "react";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+const TabIcon = ({focused, icon, text}:any) => {
+    if(focused) {
+        return (
+            <>
+                <ImageBackground source={images.highlight}
+                                 className="flex flex-row w-full flex-1 min-w-[100px] min-h-14 mt-4 justify-center
+                                  items-center rounded-full overflow-hidden ">
+                    <Image source={icon}
+                           tintColor="#151312" className="size-5"/>
+                    <Text className="text-third text-base font-semibold ml-2">{text}</Text>
+                </ImageBackground>
+
+            </>
+        )
+    }
+    return (
+        <View className={"size-full justify-center items-center mt-4 rounded-full"}>
+            <Image source={icon} tintColor={'#A8B5DB'}
+                   className="size-5"/>
+        </View>
+    )
+}
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    return(
+        <Tabs>
+            <Tabs.Screen
+                name = "index"
+                options = {{
+                    title: "Home",
+                    headerShown: false,
+                    tabBarIcon: ({focused})=> (
+                        <TabIcon focused = {focused} icon = {icons.home}  text = "Home" />
+                    )
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+                }}
+
+
+            />
+            <Tabs.Screen
+                name = "events"
+                options = {{
+                    title: "Events",
+                    headerShown: false,
+                    tabBarIcon: ({focused})=> (
+                        <TabIcon focused = {focused} icon = {icons.event}  text = "Events" />
+                    )
+
+                }}
+            />
+            <Tabs.Screen
+                name = "videos"
+                options = {{
+                    title: "Lectures",
+                    headerShown: false,
+                    tabBarIcon: ({focused})=> (
+                        <TabIcon focused = {focused} icon = {icons.openbook}  text = "Lectures" />
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name = "donation"
+                options = {{
+                    title: "Donate",
+                    headerShown: false,
+                    tabBarIcon: ({focused})=> (
+                        <TabIcon focused = {focused} icon = {icons.charity}  text = "Donate" />
+                    )
+
+                }}
+            />
+        </Tabs>
+    )
 }
---
+++ constants/icons.ts
@@ -0,0 +1,27 @@
+import home from "@/assets/icons/home.png";
+import search from "@/assets/icons/search.png";
+import person from "@/assets/icons/person.png";
+import logo from "@/assets/icons/logo.png";
+import save from "@/assets/icons/save.png";
+import star from "@/assets/icons/star.png";
+import play from "@/assets/icons/play.png";
+import arrow from "@/assets/icons/arrow.png";
+import event from "@/assets/icons/event.png";
+import openbook from "@/assets/icons/openbook.png";
+import charity from "@/assets/icons/charity.png";
+
+
+export const icons = {
+  home,
+  search,
+  person,
+  logo,
+  save,
+  star,
+  play,
+  arrow,
+  event,
+  openbook,
+  charity
+
+};
