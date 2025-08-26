7bc17bcc45dbf8126c41e8028fc1af0c081bb497
eitanabrishami <abrishami@gmail.com>
2025-08-26 13:26:43 -0700

    slight modification for donation page

--- app/(tabs)/donation.tsx
+++ app/(tabs)/donation.tsx
@@ -1,11 +1,39 @@
-import {View, Text} from "react-native";
+import {View, Text, StyleSheet} from "react-native";
 import React from "react";
 const Donation = () => {
     return(
         <View>
-            <Text>Donation</Text>
+            <Text style={styles.headingText}>Donation</Text>
+            <View style={styles.container}>
+                <View style={[styles.card, styles.cardOne]}>
+                    <Text>Red</Text>
+                </View>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    headingText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        // marginVertical: 10,
+        paddingHorizontal: 10,
+    },
+    container: {},
+    cardOne: {
+        backgroundColor: '#EF5354',
+
+    },
+    card: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 100,
+        height: 100,
+    borderRadius: 10,
+        margin: 8,
+    }
+})
 
 export default Donation;
