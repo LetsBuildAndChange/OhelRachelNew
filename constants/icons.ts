a482222c3ef235ba22952ff91635368f1015f0f5
eitanabrishami <abrishami@gmail.com>
2025-10-16 19:16:38 -0700

    added new Events Page

--- constants/icons.ts
+++ constants/icons.ts
@@ -1,13 +1,24 @@
+// @ts-ignore
 import home from "@/assets/icons/home.png";
+// @ts-ignore
 import search from "@/assets/icons/search.png";
+// @ts-ignore
 import person from "@/assets/icons/person.png";
+// @ts-ignore
 import logo from "@/assets/icons/logo.png";
+// @ts-ignore
 import save from "@/assets/icons/save.png";
+// @ts-ignore
 import star from "@/assets/icons/star.png";
+// @ts-ignore
 import play from "@/assets/icons/play.png";
+// @ts-ignore
 import arrow from "@/assets/icons/arrow.png";
+// @ts-ignore
 import event from "@/assets/icons/event.png";
+// @ts-ignore
 import openbook from "@/assets/icons/openbook.png";
+// @ts-ignore
 import charity from "@/assets/icons/charity.png";
 
 
@@ -22,6 +33,7 @@ export const icons = {
   arrow,
   event,
   openbook,
-  charity
-
+  charity,
+  torah: require("@/assets/icons/torah.png"),
+  torahbook: require("@/assets/icons/torahbook.png"),
 };
