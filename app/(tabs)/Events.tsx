cb7d2ef405a9b144d27bc47bd0b5f35341d92f48
eitanabrishami <eitanabrishami@gmail.com>
2025-10-31 16:04:40 -0700

    created layout for Event Header

--- app/(tabs)/Events.tsx
+++ app/(tabs)/Events.tsx
@@ -1,13 +1,13 @@
-import {View, ScrollView, Image} from "react-native";
+import {View, ScrollView, Image, Text} from "react-native";
 import React from "react";
 import { icons } from "@/constants/icons";
-import { Avatar, Button, Card, Text } from 'react-native-paper';
+import { Avatar, Button, Card } from 'react-native-paper';
 
 // const LeftContent = (props: any) => <icons.arrow {...props} icon="folder" />
 
-const lectureCard = (props: any) => (
-    <View>
-        <Image source={icons.event}></Image>
+function LectureCard() {
+    return(
+        <View className={"flex-1 items-center justify-center"}>
         <Text>Title</Text>
         <Text>Description</Text>
         <Text>Date</Text>
@@ -18,28 +18,39 @@ const lectureCard = (props: any) => (
         <Text>Link</Text>
         <Text>Link</Text>
     </View>
-)
+    )
+}
 
 const Events = () => {
     // @ts-ignore
     return(
 
-        <ScrollView className = "flex-1 mt-4">
-            {/*<lectureCard/>*/}
-            <Card>
-                <Card.Title title="Card Title" subtitle="Card Subtitle"/>
-                <Card.Content>
-                    <Text variant="titleLarge">Card title</Text>
-                    <Text variant="bodyMedium">Card content</Text>
-                </Card.Content>
-                <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-                <Card.Actions>
-                    <Button>Cancel</Button>
-                    <Button>Ok</Button>
-                </Card.Actions>
-            </Card>
-            <Text>Events</Text>
+        <ScrollView className = "flex-1 mt-10">
+            <View className={"flex-1 items-center justify-center"}>
+                <Image source={icons.event} className={"w-10 h-10 color-primary"}></Image>
+                <Text>Events & Classes</Text>
+                <Text className={"items-center justify-center"}>
+                    Join our weekly classes and upcoming community events</Text>
+            </View>
+            <View className={"flex-1 items-left"}>
+                <Text>Weekly Classes</Text>
+            </View>
+            <LectureCard/>
+        {/*    <Card>*/}
+        {/*        <Card.Title title="Card Title" subtitle="Card Subtitle"/>*/}
+        {/*        <Card.Content>*/}
+        {/*            /!*<Text variant="titleLarge">Card title</Text>*!/*/}
+        {/*            /!*<Text variant="bodyMedium">Card content</Text>*!/*/}
+        {/*        </Card.Content>*/}
+        {/*        <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />*/}
+        {/*        <Card.Actions>*/}
+        {/*            <Button>Cancel</Button>*/}
+        {/*            <Button>Ok</Button>*/}
+        {/*        </Card.Actions>*/}
+        {/*    </Card>*/}
+        {/*    <Text>Events</Text>*/}
         </ScrollView>
+
     )
 }
 
